2fa82102fde88995383da92b887fb40433d00f7f
Michael Fiford <michaelfiford@gmail.com>
2019-05-10 23:59:34 +0100

    Validate creation model

--- Blazor20Questions.Shared/CreateGameModel.cs
+++ Blazor20Questions.Shared/CreateGameModel.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Blazor20Questions.Shared
 {
     public class CreateGameModel
     {
+        [Required]
+        [RegularExpression("")]
         public string Subject { get; set; }
+
+        [Required]
+        [Range(1, 30, ErrorMessage = "Game must be between 1 and 30 minutes")]
         public int Minutes { get; set; }
+
+        [Required]
+        [Range(1, 50, ErrorMessage = "Must allow between 1 and 50 questions")]
         public int Questions { get; set; }
+
         public bool GuessesCountAsQuestions { get; set; }
     }
 }
