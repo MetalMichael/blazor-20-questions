c073c5fb15ac89d0fb8766a8d3391a5da863b2f0
Michael Fiford <michaelfiford@gmail.com>
2019-05-10 23:56:52 +0100

    Add MongoDB things

---
+++ Blazor20Questions.Server/Config/ServerConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazor20Questions.Server.Config
+{
+    public class ServerConfig
+    {
+        public MongoDBConfig MongoDB { get; set; } = new MongoDBConfig();
+    }
+}
--- Blazor20Questions.Server/Store/GameStore.cs
+++ Blazor20Questions.Server/Store/GameStore.cs
@@ -1,26 +1,35 @@
-﻿using Blazor20Questions.Server.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using Blazor20Questions.Server.Config;
+using Blazor20Questions.Server.Models;
 
 namespace Blazor20Questions.Server.Store
 {
     public class GameStore : IGameStore
     {
+        private readonly IMongoDatabase _db;
+        public GameStore(MongoDBConfig config)
+        {
+            var client = new MongoClient(config.ConnectionString);
+            _db = client.GetDatabase(config.Database);
+        }
+
+        private IMongoCollection<GameModel> _games => _db.GetCollection<GameModel>("Games");
+
         public async Task CreateNewGame(GameModel model)
         {
-
+            await _games.InsertOneAsync(model);
         }
 
         public async Task<GameModel> GetGame(Guid id)
         {
-            return null;
+            return await _games.Find(g => g.Id == id).FirstAsync();
         }
 
         public async Task UpdateGame(GameModel model)
         {
-
+            await _games.ReplaceOneAsync(g => g.Id == model.Id, model);
         }
     }
 }
