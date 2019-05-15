ebaf8d717af7154bdffcc2939a0af15676858fae
MetalMichael <michaelfiford@gmail.com>
2019-05-16 00:20:15 +0100

    Add base component, and abstract Host

---
+++ Blazor20Questions.Client/Pages/PlayerComponent.cs
@@ -0,0 +1,60 @@
+﻿using Blazor.Extensions;
+using Blazor20Questions.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blazor20Questions.Client.Pages
+{
+    public abstract class PlayerComponent : ComponentBase
+    {
+        [Inject]
+        protected HttpClient Http { get; set; }
+        [Inject]
+        protected IJSRuntime JsRuntime { get; set; }
+
+        [Parameter]
+        public Guid Id { get; set; }
+
+        protected GameResponse _game;
+
+        protected bool _error;
+        protected string _errorMessage;
+        protected HubConnection _connection;
+
+        protected override async Task OnInitAsync()
+        {
+            try
+            {
+                _error = false;
+                _game = await Http.GetJsonAsync<GameResponse>($"api/Game/{Id}");
+
+                await SetupRefresh();
+            }
+            catch (Exception e)
+            {
+                _error = true;
+                _errorMessage = e.Message;
+            }
+        }
+
+        private async Task SetupRefresh()
+        {
+            _connection = new HubConnectionBuilder(JsRuntime).WithUrl("/hubs/game").Build();
+            _connection.On<string>("UpdateGame", UpdateGame);
+            await _connection.StartAsync();
+            await _connection.InvokeAsync("Register", Id);
+        }
+
+        private Task UpdateGame(string game)
+        {
+            _game = JsonConvert.DeserializeObject<GameResponse>(game);
+            StateHasChanged();
+            Console.WriteLine("Updated game");
+            return Task.CompletedTask;
+        }
+    }
+}
